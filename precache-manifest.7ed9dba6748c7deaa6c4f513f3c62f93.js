19778a40d69aecd1cac8e7ac4b2af9ffdd989200
Mike Heyman <mheyman@ITO-KOLOA.dlabs.com>
2019-08-04 11:31:46 -1000

    Updates

---
+++ precache-manifest.7ed9dba6748c7deaa6c4f513f3c62f93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "3f7810326dcb1d903fb9",
+    "url": "/test/static/js/main.3f781032.chunk.js"
+  },
+  {
+    "revision": "c77df5867bd34b389b0b",
+    "url": "/test/static/js/1.c77df586.chunk.js"
+  },
+  {
+    "revision": "3f7810326dcb1d903fb9",
+    "url": "/test/static/css/main.31548369.chunk.css"
+  },
+  {
+    "revision": "b028f752b2d774c79b2af7bbf6b922f5",
+    "url": "/test/index.html"
+  }
+];
